c3eb60e1120964d2e0e093f524a9f5fee6d29840
Danik Vitek <x3665107@gmail.com>
2023-09-30 17:35:42 +0300

    implement `locale` resource

--- src/stores/translationStore.ts
+++ src/stores/translationStore.ts
@@ -1,25 +1,50 @@
-import { fs, path } from "@tauri-apps/api"
-import { BaseDirectory } from "@tauri-apps/api/fs"
-import { createMemo, createResource, createSignal } from "solid-js"
+import { fs } from "@tauri-apps/api";
+import { BaseDirectory } from "@tauri-apps/api/fs";
+import { createResource, createSignal } from "solid-js";
 
 export interface Translation {
-    "disks.disks": string
-    "files.name": string
-    "files.createdAt": string
-    "files.modifiedAt": string
-    "files.size": string
+    "disks.disks": string;
+    "files.name": string;
+    "files.createdAt": string;
+    "files.modifiedAt": string;
+    "files.size": string;
 }
 
-export const [localeId, setLocaleId] = createSignal("en-US")
-export const [locale] = createResource<Translation, string>(localeId, async (source, { }) => {
-    let fallbackLocaleId = "en-US";
-    let localeId = source
-    if (!(await fs.exists(`locales`, { dir: BaseDirectory.AppConfig }))) {
-        await fs.createDir("locales", { dir: BaseDirectory.AppConfig })
-    }
-    if (!(await fs.exists(`locales/${localeId}.json`, { dir: BaseDirectory.AppConfig }))) {
-        localeId = fallbackLocaleId
-        await fs.writeTextFile(`locales/${localeId}.json`, await fs.readTextFile(`locales/${localeId}.json`, { dir: BaseDirectory.Resource }), { dir: BaseDirectory.AppConfig })
-    }
-    return JSON.parse(await fs.readTextFile(`locales/${localeId}.json`, { dir: BaseDirectory.AppConfig })) as Translation
-})
+export const fallbackLocaleId = "en-US";
+const fallbackLocalePath = `locales/${fallbackLocaleId}.json`;
+
+export const [localeId, setLocaleId] = createSignal(fallbackLocaleId);
+
+export const [locale] = createResource<Translation, string>(
+    localeId,
+    async (localeId, {}) => {
+        const localeDirExists = await fs.exists("locales", {
+            dir: BaseDirectory.AppConfig,
+        });
+        if (!localeDirExists) {
+            await fs.createDir("locales", { dir: BaseDirectory.AppConfig });
+        }
+
+        const localePath = `locales/${localeId}.json`;
+        const localeExists = await fs.exists(localePath, {
+            dir: BaseDirectory.AppConfig,
+        });
+
+        let localeJson: string;
+        if (localeExists) {
+            localeJson = await fs.readTextFile(localePath, {
+                dir: BaseDirectory.AppConfig,
+            });
+        } else {
+            localeJson = await fs.readTextFile(fallbackLocalePath, {
+                dir: BaseDirectory.Resource,
+            });
+            // write fallback locale into the specified locale file
+            await fs.writeTextFile(localePath, localeJson, {
+                dir: BaseDirectory.AppConfig,
+            });
+        }
+
+        return JSON.parse(localeJson) as Translation;
+    },
+);
